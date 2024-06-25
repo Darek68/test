f4912cd3e246e8ff1aab282e8c7f5033639eec32
Darek <darek68@list.ru>
2024-06-25 22:52:25 +0300

    FirstCommitTest

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Client {
+
+    @Id
+    private UUID id;
+
+    @Column(name = "NAME", length = 25,nullable = false)
+    private String name;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public Client(String imie) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,35 @@
+package org.example;
+
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Hello and welcome!");
+        persistDemo();
+
+    }
+    public static void persistDemo() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("SingleUnit");
+        EntityManager entityManager = emf.createEntityManager();
+        Client client = new Client("Imie");
+
+        EntityTransaction transaction = entityManager.getTransaction();
+//        Выполняется открытие транзакции
+        transaction.begin();
+//        Объект помещается в Persistence Context - INSERT не выполняется
+        entityManager.persist(client);
+//        Объект (тот же самый) возвращается из Persistent Context - SELECT не выполняется
+      //  Client clientFromContext = entityManager.find(Client.class, client.getId());
+        Client clientFromContext = entityManager.find(Client.class, client.getId());
+//        Выполняется коммит транзакции - выполняется INSERT
+        transaction.commit();
+        entityManager.close();
+    }
+}
